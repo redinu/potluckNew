2449d3cbbf9587d0ff9eb167d3c20cab15f7c0dd
Rediet Ayalew <rediet.d.ayalew@gmail.com>
2017-06-26 14:04:16 -0400

    final

---
+++ src/main/java/com/example/demo/Registry.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Registry {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
+	private String name;
+	@NotNull
+	private String food;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFood() {
+		return food;
+	}
+	public void setFood(String food) {
+		this.food = food;
+	}
+	
+	
+	
+}
